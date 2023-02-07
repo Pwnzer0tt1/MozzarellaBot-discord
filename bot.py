acdcf488fade4fe05be104b0d8ec221495299f74
DomySh <me@domysh.com>
2023-02-07 02:13:25 +0100

    Admin function and token generations

--- bot.py
+++ bot.py
@@ -1,28 +1,115 @@
 # bot.py
-import os, asyncio
-import discord
-from syjson import SyJsonpierantonio.dagostino@gmail.com
+import os, discord, secrets, asyncio, traceback
+from syjson import SyJson
 from dotenv import load_dotenv
+from discord import app_commands
 load_dotenv()
+
+"""
+Create Admin Function to revoke a token or a list of tokens
+Create the function to send via email the tokens
+"""
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 db = SyJson("db.json")
-
 class g:
     guild = None
     auth_ch_name = "🔐︱auth"
+    synced = False
+    
+cmd = app_commands.CommandTree(client)
+
+
+gen_token_lock = asyncio.Lock()
+async def gen_token():
+    async with gen_token_lock:
+        while True:
+            tk = secrets.token_hex(16)
+            if tk not in db["auth_tokens"]:
+                db["auth_tokens"][tk] = None
+                return tk
+
+async def add_token_role(roles):
+    roles = list(map(int, roles))
+    tk = await gen_token()
+    db["auth_tokens"][tk] = {"role":roles,"type":"role"}
+    return tk
+
+class GenTokensModal(discord.ui.Modal):
+    def __init__(self, roles):
+        super().__init__(title="Generate Tokens")
+        self.roles = roles
+        self.add_item(discord.ui.TextInput(label="Insert the Amount"))
+        
+    async def on_submit(self, interaction):
+        amount = interaction.data["components"][0]["components"][0]["value"]
+        if not isinstance(amount,str) or not amount.isdigit():
+            await interaction.response.edit_message("Please insert a valid amount")
+            return
+        tokens = [await add_token_role(self.roles) for _ in range(int(amount))]
+        token_text = "\n".join(tokens)
+        await interaction.response.edit_message(content=f"Here are your generated tokens:\n```\n{token_text}\n```\nSave the tokens before closing this message!", view=None)
+
+class GenTokensView(discord.ui.View):
+    def __init__(self, roles):
+        super().__init__()
+        self.roles = roles
+        self.btn = discord.ui.Button(label="Open", style=discord.ButtonStyle.primary)
+        self.add_item(self.btn)
+        async def callback(interaction):
+            await interaction.response.send_modal(GenTokensModal(roles))
+        self.btn.callback = callback
+
+class GenTokensBtn(discord.ui.Button):
+    def __init__(self):
+        super().__init__(label="Generate Tokens", style=discord.ButtonStyle.primary)
+        
+    async def callback(self, interaction):
+        view = discord.ui.View()
+        select_roles = discord.ui.RoleSelect(max_values=10)
+        view.add_item(select_roles)
+        async def select_roles_callback(interaction):
+            await interaction.response.edit_message(content="Insert the amount of tokens to generate", view=GenTokensView(interaction.data["values"]))
+        select_roles.callback = select_roles_callback
+        await interaction.response.edit_message(content="Select the role to add with tokens", view=view)
+
+class AdminView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(GenTokensBtn())
+
+@cmd.command(name="admin", description="Admin commands")
+@app_commands.default_permissions(administrator=True)
+@app_commands.checks.has_permissions(administrator=True)
+async def gen_auth_token(interaction):
+    await interaction.response.send_message("Click a button to activate an admin function", view=AdminView(), ephemeral=True)
+
+@gen_auth_token.error
+async def gen_auth_token_error(interaction, error):
+    if isinstance(error, app_commands.errors.MissingPermissions):
+        await interaction.response.send_message("You don't have the permissions to use this command", ephemeral=True)
+    else:
+        await interaction.response.send_message("Unknown error", ephemeral=True)
+
 
 async def action_handler(action, interaction):
     if action["type"] == "role":
         roles = []
-        if isinstance(action["role"],list):
-            roles = [discord.utils.get(g.guild.roles,name=role) for role in action["role"]]
-        else:
-            roles = [discord.utils.get(g.guild.roles,name=action["role"])]
-        [await interaction.user.add_roles(ele) for ele in roles]
-        return f"Authenticated successfully with role {action['role']} :), Welcome to the server!"
+        try:
+            if isinstance(action["role"],list):
+                roles = [discord.utils.get(g.guild.roles,id=int(role)) for role in action["role"]]
+            else:
+                roles = [discord.utils.get(g.guild.roles,id=int(action["role"]))]
+        except Exception:
+            return "There was a problem trying to add the role(s) :/, if it's necessary, contact the admin, the token has been invalidated :("
+        for ele in roles:
+            try:
+                await interaction.user.add_roles(ele) 
+            except Exception:
+                pass
+        return f"Authenticated successfully with role(s) `{'` `'.join([r.name for r in roles])}`, Welcome to the server!"
     else:
         return "Error: Unknown action 0_0"
 
@@ -30,8 +117,7 @@ async def action_handler(action, interaction):
 class AuthModal(discord.ui.Modal):
     def __init__(self):
         super().__init__(title="Authentication")
-        token_input = discord.ui.TextInput(label="Insert the Token")
-        self.add_item(token_input)
+        self.add_item(discord.ui.TextInput(label="Insert the Token"))
     
     async def on_submit(self, interaction):
         token = interaction.data["components"][0]["components"][0]["value"]
@@ -78,6 +164,8 @@ async def on_ready():
     db.create("auth_tokens",{})
     g.guild = client.guilds[0]
     await check_and_create_authchannel()
-    
+    if not g.synced:
+        await cmd.sync()
+        g.synced = True
 
 client.run(TOKEN)
