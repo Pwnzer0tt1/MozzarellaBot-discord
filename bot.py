3c9a273dbc85ed37d28453d6ee77a354e159eeb4
DomySh <me@domysh.com>
2023-02-17 03:57:26 +0100

    Create auth on command request

--- bot.py
+++ bot.py
@@ -8,21 +8,33 @@ client = discord.Client(intents=discord.Intents.default())
     
 cmd = app_commands.CommandTree(client)
 
+async def reset_auth_channel(channel):
+    await channel.set_permissions(g.guild.default_role, send_messages=False, read_messages=True)
+    await channel.purge()
+    await channel.send("Welcome to the auth channel, please send your auth token here", view=AuthView())
+    
+
 @cmd.command(name="admin", description="Admin commands")
 @app_commands.default_permissions(administrator=True)
 @app_commands.checks.has_permissions(administrator=True)
 async def gen_auth_token(interaction):
     await interaction.response.send_message("Click a button to activate an admin function", view=AdminView(), ephemeral=True)
 
+@cmd.command(name="regen_auth_channel", description="Regenerate the auth channel")
+@app_commands.default_permissions(administrator=True)
+@app_commands.checks.has_permissions(administrator=True)
+async def gen_auth_ch(interaction):
+    await check_and_create_authchannel()
+    await interaction.response.send_message("The Auth channel hash been regenerated!", ephemeral=True)
+
 @gen_auth_token.error
 async def gen_auth_token_error(interaction, error):
     await error_handler(interaction, error)
 
-async def reset_auth_channel(channel):
-    await channel.set_permissions(g.guild.default_role, send_messages=False, read_messages=True)
-    await channel.purge()
-    await channel.send("Welcome to the auth channel, please send your auth token here", view=AuthView())
-    
+@gen_auth_ch.error
+async def gen_auth_ch_error(interaction, error):
+    await error_handler(interaction, error)
+
 async def check_and_create_authchannel():
     for channel in g.guild.channels:
         if channel.name == g.auth_ch_name:
@@ -40,7 +52,6 @@ async def on_ready():
         exit(1)
     db.create("auth_tokens",{})
     g.guild = client.guilds[0]
-    await check_and_create_authchannel()
     if not g.synced:
         await cmd.sync()
         g.synced = True
